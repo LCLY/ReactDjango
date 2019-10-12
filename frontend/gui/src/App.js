a550ae67ec70c3192263f3a406f25adc91065793
lchoo <lchoo1996@gmail.com>
2019-10-12 16:03:04 -0700

    refactor: setup frontend and backup with new layout

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./App.css";
+import CustomLayout from "./containers/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CustomLayout></CustomLayout>
+        </div>
+    );
 }
 
 export default App;
