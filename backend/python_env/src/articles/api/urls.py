9b70c87f515f63dd4065823d95955c3b10d2ce35
lchoo <lchoo1996@gmail.com>
2019-10-14 01:26:40 -0700

    refactor: added viewsets, replacing general views

--- backend/python_env/src/articles/api/urls.py
+++ backend/python_env/src/articles/api/urls.py
@@ -1,8 +1,26 @@
-from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import ArticleListView, ArticleDetailView
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view())
-]
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, basename='articles')
+urlpatterns = router.urls
+
+# from django.urls import path
+
+# from .views import (
+#     ArticleListView,
+#     ArticleDetailView,
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDeleteView,
+# )
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('<pk>/update/', ArticleUpdateView.as_view()),
+#     path('<pk>/delete/', ArticleDeleteView.as_view()),
+
+# ]
