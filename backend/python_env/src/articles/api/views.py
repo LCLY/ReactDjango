708cb413a0fdb1485db0d935dd2a217e7fe8dbff
lchoo <lchoo1996@gmail.com>
2019-10-14 01:26:55 -0700

    refactor: replace generic with viewsets in views.py

--- backend/python_env/src/articles/api/views.py
+++ backend/python_env/src/articles/api/views.py
@@ -1,14 +1,45 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+# this is an easier and cleaner way compare to the one before
+from rest_framework import viewsets
 
 from articles.models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
 
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView
+# )
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# # a post method api, remember to import in urls.py
+
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
