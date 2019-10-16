fe3f848be2fbbb805171c2a2dcc08f6d6f30aee4
lchoo <lchoo1996@gmail.com>
2019-10-16 15:08:28 -0700

    refactor: check authenticaton state

--- frontend/gui/src/store/actions/auth.js
+++ frontend/gui/src/store/actions/auth.js
@@ -91,3 +91,28 @@ export const authSignup = (username, email, password1, password2) => {
             });
     };
 };
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem("token");
+        if (token === undefined) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(
+                localStorage.getItem("expirationDate"),
+            );
+            // if its old, logout
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            } else {
+                dispatch(authSuccess(token));
+                dispatch(
+                    checkAuthTimeout(
+                        (expirationDate.getTime() - new Date().getTime()) /
+                            1000,
+                    ),
+                );
+            }
+        }
+    };
+};
