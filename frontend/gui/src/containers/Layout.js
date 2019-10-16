261e6f125847d8cc50bc65a30f4504a0278c5823
lchoo <lchoo1996@gmail.com>
2019-10-16 03:19:04 -0700

    refactor: added redux

--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -13,9 +13,12 @@ const CustomLayout = props => {
                     defaultSelectedKeys={["2"]}
                     style={{ lineHeight: "64px" }}
                 >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="1">
+                        <Link to="/">Posts</Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to="/login">Login</Link>
+                    </Menu.Item>
                 </Menu>
             </Header>
             <Content style={{ padding: "0 50px" }}>
