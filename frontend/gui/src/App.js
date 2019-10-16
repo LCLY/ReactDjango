7967f9de9f0c8923e8a8ca434dd772dc45e81066
lchoo <lchoo1996@gmail.com>
2019-10-16 04:38:49 -0700

    refactor: change app to class component

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import CustomLayout from "./containers/Layout";
 import BaseRouter from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
-function App() {
-    return (
-        <div>
-            <Router>
-                <CustomLayout>
-                    <BaseRouter />
-                </CustomLayout>
-            </Router>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <CustomLayout>
+                        <BaseRouter />
+                    </CustomLayout>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
