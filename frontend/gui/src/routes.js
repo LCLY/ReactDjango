6b447566c0e3e2fc9f0f60a00f72de609a094d71
lchoo <lchoo1996@gmail.com>
2019-10-16 18:28:12 -0700

    refactor: added new route

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ArticleListView from "./containers/ArticleListView";
 import ArticleDetailView from "./containers/ArticleDetailView";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 const BaseRouter = () => {
     return (
         <div>
@@ -9,7 +11,13 @@ const BaseRouter = () => {
             <Route exact path="/" component={ArticleListView} />
             {/* detail */}
             {/* now it will only show respective article detail according to the id */}
-            <Route exact path="/:articleID" component={ArticleDetailView} />
+            <Route
+                exact
+                path="/articles/:articleID/"
+                component={ArticleDetailView}
+            />
+            <Route exact path="/login/" component={Login} />
+            <Route exact path="/signup/" component={signup} />
         </div>
     );
 };
